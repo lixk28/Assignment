6aab52546f9a1757efab111a17b95f17fe730800
Niklaus <niklausama@qq.com>
2021-02-10 01:20:08 +0800

    Update test

--- test.cpp
+++ test.cpp
@@ -4,7 +4,7 @@
  * Author       : Niklaus
  * Date         : 2021-02-09 21:30:28
  * LastEditor   : Niklaus
- * LastEditTime : 2021-02-09 21:43:21
+ * LastEditTime : 2021-02-10 01:19:20
  * 
  * Copyright (C) 2021 Niklaus. All rights reserved.
  **********************************************************/
@@ -14,6 +14,7 @@ using namespace std;
 int main()
 {
     cout << "This is a test for waka readme." << endl;
-    
+    cout << "Test Test and Test!" << endl;
+
     return 0;
 }
