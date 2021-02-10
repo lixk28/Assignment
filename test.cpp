adf73d3524c2b4ec35bdafcd0a97dc4ee59ebdbf
Niklaus <niklausama@qq.com>
2021-02-10 15:41:27 +0800

    test for VSCode

--- test.cpp
+++ test.cpp
@@ -4,11 +4,7 @@
  * Author       : Niklaus
  * Date         : 2021-02-09 21:30:28
  * LastEditor   : Niklaus
-<<<<<<< HEAD
- * LastEditTime : 2021-02-10 01:27:29
-=======
- * LastEditTime : 2021-02-10 01:22:51
->>>>>>> fc9d2a96e75312cd50876d0f818fd29eb1abc0f2
+ * LastEditTime : 2021-02-10 15:39:27
  * 
  * Copyright (C) 2021 Niklaus. All rights reserved.
  **********************************************************/
@@ -20,10 +16,7 @@ int main()
     cout << "This is a test for waka readme." << endl;
     cout << "Test Test and Test!" << endl;
     cout << "What the HACK" << endl;
-<<<<<<< HEAD
     cout << "Test" << endl;
-=======
->>>>>>> fc9d2a96e75312cd50876d0f818fd29eb1abc0f2
 
     return 0;
 }
